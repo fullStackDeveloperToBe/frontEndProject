0c6e97e969d962c06590e52e926b96707b14db8f
fullStackDeveloperToBe <gowdadushyanth27@gmail.com>
2022-04-01 18:20:47 +0530

    adding components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,31 @@
 import React, { Component } from 'react'
-import {Nav, Navbar, NavDropdown, NavItem, MenuItem} from 'react-bootstrap';
+// import { Nav } from 'react-bootstrap';
 
 export class Home extends Component {
   render() {
-    return (
+    return (  
         <div>
       <div>Home</div>
-      <Nav defaultActiveKey="/home" as="ul">
-  <Nav.Item as="li">
-    <Nav.Link href="/home">Active</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link eventKey="link-1">Link</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link eventKey="link-2">Link</Nav.Link>
-  </Nav.Item>
-</Nav>
+      <table>
+          <tr>
+              <td>q</td>
+              <td>q</td>
+              <td>q</td>
+              <td>q</td>
+          </tr>
+          <tr>
+              <td>q</td>
+              <td>q</td>
+              <td>q</td>
+              <td>q</td>
+          </tr>
+          <tr>
+              <td>q</td>
+              <td>q</td>
+              <td>q</td>
+              <td>q</td>
+          </tr>
+      </table>
       </div>
     )
   }
