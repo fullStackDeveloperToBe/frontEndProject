54809cb46738af88bf80ca22688a105fc456d9f2
fullStackDeveloperToBe <99525378+fullStackDeveloperToBe@users.noreply.github.com>
2022-06-01 14:37:43 +0530

    connected with backend

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import React from 'react';
 //import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home } from './components/Home'
+import UserService from './services/UserService';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Router>
-          <Route exact path = '' component={Home}></Route>
+          <Route exact path = '' component={UserService}></Route>
         </Router>
       </header>
     </div>
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,13 +1,23 @@
 import axios from "axios";
+import { useEffect } from "react";
+const history = require("history").createBrowserHistory()
 
-const USER_API = "http://localhost:8081/employees";
+const USER_API = "http://localhost:8081/employees/";
 
 
+// const USER_API = "https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001";
 
-class UserService  {
-    getAllEmployee(){
-         axios.get(USER_API);
-    }
+function UserService() {
+    useEffect(() => {
+        axios
+            .get(USER_API)
+            .then((res) => {console.log(res.data)});
+    });
+
+    return <div>Hello World</div>
+
+
 }
 
-export default new UserService()
+export default UserService
+
